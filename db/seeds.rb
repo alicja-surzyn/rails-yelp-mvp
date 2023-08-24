04c7831a7119bc31bb913d39dadf4143eb9d08b5
Alicja Surzyn <alicja.surzyn@gmail.com>
2023-08-23 19:35:31 -0600

    "Rails Yelp"

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Choclo_y_maiz = { name: "Choclo y maiz", address: "Americana", phone_number: "3312345678", category: "belgian"}
+Sinomino = {name: "Sinomino", address: "Americana", phone_number: "3312345670", category: "french"}
+Mermelada = {name: "Mermelada", address: "Providencia", phone_number: "3312345677", category: "chinese"}
+Caligari = {name: "Caligari", address: "Americana", phone_number: "3392345670", category: "french"}
+
+[Choclo_y_maiz, Sinomino, Mermelada, Caligari].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
